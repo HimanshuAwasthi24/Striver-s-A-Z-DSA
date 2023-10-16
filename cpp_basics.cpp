0db68655230108be7cd0b931b80a0bb2309933ea
vaidehi7007 <awasthineha2001@gmail.com>
2023-10-16 18:15:52 +0530

    Conditions

--- cpp_basics.cpp
+++ cpp_basics.cpp
@@ -1,10 +1,22 @@
-//skeleton of a program
-#include<bits/stdc++.h> //bits/stdc++.h includes all libraries inside it
+// skeleton of a program
+#include <bits/stdc++.h> //bits/stdc++.h includes all libraries inside it
 using namespace std;
-//using std as name space
-int main(){
+// using std as name space
+int main()
+{
+    // input output
     int n;
-    cout << "enter a number\n";
+    string s;
+    cout << "Enter Name\n";
+    getline(cin, s);
+    cout << s << endl;
+    cout << "enter the age of " << s << endl;
     cin >> n;
     cout << n << endl;
+    // WAP to check adult or not
+    if (n >= 18)
+        cout << s << " is an adult\n";
+    else
+        cout << "He is not an adult\n";
+    return 0;
 }
