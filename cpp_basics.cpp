3e3c028e62e49e58ae51081fd7b7383600279d14
vaidehi7007 <awasthineha2001@gmail.com>
2023-10-16 18:02:16 +0530

    basics

---
+++ cpp_basics.cpp
@@ -0,0 +1,10 @@
+//skeleton of a program
+#include<bits/stdc++.h> //bits/stdc++.h includes all libraries inside it
+using namespace std;
+//using std as name space
+int main(){
+    int n;
+    cout << "enter a number\n";
+    cin >> n;
+    cout << n << endl;
+}
